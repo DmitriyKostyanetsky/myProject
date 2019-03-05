561ed8a3bc0f5c2eec0fee04d536b22b30b25dd9
Dmitriy <onlywarinfarfuture@gmail.com>
2019-03-06 00:49:02 +0300

    Homework 1(correction 1)

--- src/main/java/aplana/Solution.java
+++ src/main/java/aplana/Solution.java
@@ -15,37 +15,57 @@ public class Solution {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Enter number of task: (1 - calculator, 2 - string array)" );
-        Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt();
+        while (true) {
+            System.out.println("Enter number of task: (1 - calculator, 2 - string array, 0 - exit)" );
+            Scanner scanner = new Scanner(System.in);
 
-        if (i == 1) {
-            System.out.println("Enter operation: (1 - sum, 2 - subtract, 3 - multiple, 4 - divide)");
-            int choose = scanner.nextInt();
-            Calculator calc = new Calculator();
-            double result;
+            if (!scanner.hasNextDouble()) {
+                System.out.println("Incorrect value. Please input positive number");
+            } else {
+                int i = scanner.nextInt();
 
-            switch (choose) {
-                case 1: result = calc.sum();
-                    System.out.printf("Sum is : %.4f", result);
+                if (i == 1) {
+                    System.out.println("Enter operation: (1 - sum, 2 - subtract, 3 - multiple, 4 - divide)");
+                    if (!scanner.hasNextDouble()) {
+                        System.out.println("Incorrect value. Please input positive number");
+                    } else {
+                        int choose = scanner.nextInt();
+                        Calculator calc = new Calculator();
+                        double result;
+
+                        switch (choose) {
+                            case 1:
+                                result = calc.sum();
+                                System.out.printf("Sum is : %.4f", result);
+                                break;
+                            case 2:
+                                result = calc.subtract();
+                                System.out.printf("Subtract is : %.4f", result);
+                                break;
+                            case 3:
+                                result = calc.multiply();
+                                System.out.printf("Multiply is : %.4f", result);
+                                break;
+                            case 4:
+                                result = calc.divide();
+                                System.out.printf("Divide is : %.4f", result);
+                                break;
+                            default:
+                                System.err.println("Incorrect value");
+                        }
+                    }
+                }
+
+                if (i == 2) {
+                    Max max = new Max();
+                    max.searchMaxWord();
+                }
+
+                if (i == 0) {
+                    scanner.close();
                     break;
-                case 2: result = calc.subtract();
-                    System.out.printf("Subtract is : %.4f", result);
-                    break;
-                case 3: result = calc.multiply();
-                    System.out.printf("Multiply is : %.4f", result);
-                    break;
-                case 4: result = calc.divide();
-                    System.out.printf("Divide is : %.4f", result);
-                    break;
-                default:
-                    System.err.println("Incorrect value");
+                }
             }
-        } else if (i == 2) {
-            Max max = new Max();
-            max.searchMaxWord();
-        } else {
-            System.err.println("Incorrect value");
         }
     }
 }
